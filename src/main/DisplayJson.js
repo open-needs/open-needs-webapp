a7e5e1ea46aeb4500993fefe10ac1e9749c2f617
Marco Heinemann <marco.heinemann@useblocks.com>
2022-05-16 20:49:23 +0200

    Added react-json-view, downgraded to React 17
    react-json-view is not yet available for React 18.
    See https://github.com/mac-s-g/react-json-view/issues/403

--- src/main/DisplayJson.js
+++ src/main/DisplayJson.js
@@ -1,23 +1,48 @@
 import * as React from 'react';
 
-import Box from '@mui/material/Container';
-import JSONPretty from 'react-json-pretty';
-import Paper from '@mui/material/Paper';
-import { needsJsonAtom } from '../shared/atoms';
+import { colorModeAtom, needsJsonAtom } from '../shared/atoms';
+
+import { Paper } from '@mui/material';
+// import Box from '@mui/material/Container';
+// import JSONPretty from 'react-json-pretty';
+// import Paper from '@mui/material/Paper';
+import ReactJson from 'react-json-view';
+import Typography from '@mui/material/Typography';
 import { useRecoilValue } from 'recoil';
 
 export default function DisplayJson() {
+  const colorMode = useRecoilValue(colorModeAtom);
   const needsJson = useRecoilValue(needsJsonAtom);
-  const displayContent = needsJson ? needsJson : 'No needs.json loaded';
-  return (
-    <Paper elevation={3} sx={{ height: 200 }}>
-      <Box
-        disableGutters
-        component="div"
-        sx={{ height: 200, overflow: 'auto', pl: 1, mr: 0, ml: 0 }}
-      >
-        <JSONPretty id="json-pretty" data={displayContent}></JSONPretty>
-      </Box>
+  // const displayContent = needsJson ? needsJson : 'No needs.json loaded';
+  return needsJson ? (
+    <Paper elevation={3} sx={{ overflowY: 'auto', maxHeight: '500px' }}>
+      <ReactJson
+        style={{ padding: '8px' }}
+        theme={colorMode === 'light' ? 'rjv-default' : 'monokai'}
+        collapsed={3}
+        displayDataTypes={false}
+        enableClipboard={false}
+        src={needsJson}
+      />
     </Paper>
+  ) : (
+    <Typography
+      variant="h6"
+      sx={{
+        fontWeight: 100
+      }}
+    >
+      No needs.json loaded
+    </Typography>
   );
+
+  // <Paper elevation={3} sx={{ height: 200 }}>
+  //   <Box
+  //     disableGutters
+  //     component="div"
+  //     sx={{ height: 200, overflow: 'auto', pl: 1, mr: 0, ml: 0 }}
+  //   >
+  //     <JSONPretty id="json-pretty" data={displayContent}></JSONPretty>
+  //   </Box>
+  // </Paper>
 }
