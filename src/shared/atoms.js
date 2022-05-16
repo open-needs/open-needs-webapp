31979ea52cda30a09f677181f0c318334ec795ce
Marco Heinemann <marco.heinemann@useblocks.com>
2022-05-16 22:58:03 +0200

    Remember color mode
    Need to force install because recoil-persists
    depends on an older version of recoil.
    This however should not be an issue, see
    https://github.com/polemius/recoil-persist/issues/54

--- src/shared/atoms.js
+++ src/shared/atoms.js
@@ -1,8 +1,12 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist({ key: 'persist' });
 
 export const colorModeAtom = atom({
   key: 'colorMode',
-  default: 'light'
+  default: 'light',
+  effects_UNSTABLE: [persistAtom]
 });
 
 export const needsJsonAtom = atom({
