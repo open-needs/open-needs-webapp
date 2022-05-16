03f13d3c7aa1604aacba6c0e5159f9a8612ada8a
Marco Heinemann <marco.heinemann@useblocks.com>
2022-05-16 15:25:37 +0200

    Left align MainLayout

--- src/main/MainLayout.js
+++ src/main/MainLayout.js
@@ -1,45 +1,33 @@
 import * as React from 'react';
 
+import Box from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary
-}));
 
 export default function MainLayout() {
   return (
-    <Container
+    <Box
       sx={{
         mt: 3,
         maxWidth: 'lg'
       }}
     >
-      <Stack spacing={2}>
-        <Typography variant="h4">Query need items</Typography>
-        <Button
-          variant="contained"
-          component="label"
-          sx={{
-            width: '200px',
-            mt: 18
-          }}
-        >
-          Upload needs.jon
-          <input type="file" accept="application/json" hidden />
-        </Button>
-        <Item>Item 1</Item>
-        <Item>Item 2</Item>
-        <Item>Item 3</Item>
-      </Stack>
-    </Container>
+      <Typography variant="h4">Query need items</Typography>
+      <Button
+        variant="contained"
+        component="label"
+        sx={{
+          width: '200px',
+          mt: 3
+        }}
+      >
+        Upload needs.jon
+        <input type="file" accept="application/json" hidden />
+      </Button>
+      <Paper sx={{ mt: 3 }}>Item 1</Paper>
+      <Paper>Item 2</Paper>
+      <Paper>Item 3</Paper>
+    </Box>
   );
 }
