214d001feeb88c82f61bf8472c5754ab9e27ba51
Marco Heinemann <marco.heinemann@useblocks.com>
2022-07-04 09:41:58 +0200

    Removed unused atoms

--- src/shared/atoms.js
+++ src/shared/atoms.js
@@ -4,34 +4,26 @@ import { recoilPersist } from 'recoil-persist';
 
 const { persistAtom } = recoilPersist({ key: 'persist' });
 
+// persistent atoms
 export const colorModeAtom = atom({
   key: 'colorMode',
   default: 'light',
   effects_UNSTABLE: [persistAtom]
 });
 
+export const remoteBaseUrlAtom = atom({
+  key: 'remoteBaseUrl',
+  default: be_server,
+  effects_UNSTABLE: [persistAtom]
+});
+
+// non-persistent atoms
 export const needsJsonAtom = atom({
   key: 'needsJson',
   default: undefined
 });
 
-export const authOpenAtom = atom({
-  key: 'authOpen',
-  default: false
-});
-
 export const isRemoteAtom = atom({
   key: 'isRemote',
   default: false
-});
-
-export const showErrorMessageAtom = atom({
-  key: 'showErrorMessage',
-  default: null
-});
-
-export const remoteBaseUrlAtom = atom({
-  key: 'remoteBaseUrl',
-  default: be_server,
-  effects_UNSTABLE: [persistAtom]
 });
