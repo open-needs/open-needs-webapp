5645b8aa2111e77fee34f0608cf1ff07aad885af
Marco Heinemann <marco.heinemann@useblocks.com>
2022-07-04 18:00:41 +0200

    Fixed nested headline

--- src/main/AuthUi/FullWidth.jsx
+++ src/main/AuthUi/FullWidth.jsx
@@ -9,9 +9,13 @@ const FullWidthAuth = ({ ...props }) => {
         <Box boxShadow="0px 0px 10px 3px #ddd" p={3} width="100%" height="100%">
           <Grid container spacing={1}>
             <Grid item xs={false} md={7}>
-              <Typography variant="h4" color="textSecondary">
-                {props.welcomeComponent ? props.welcomeComponent : <b>Welcome</b>}
-              </Typography>
+              {props.welcomeComponent ? (
+                props.welcomeComponent
+              ) : (
+                <Typography variant="h4" color="textSecondary">
+                  <b>Welcome</b>{' '}
+                </Typography>
+              )}
             </Grid>
             <Grid item xs={12} md={5}>
               <TabAuth {...props} />
