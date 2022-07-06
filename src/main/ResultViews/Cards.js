59f1e23d7b80937f9635be5ea564eab6e018d7d6
Marco Heinemann <marco.heinemann@useblocks.com>
2022-07-06 19:24:33 +0200

    Modal Monaco window on Card click

--- src/main/ResultViews/Cards.js
+++ src/main/ResultViews/Cards.js
@@ -1,9 +1,11 @@
-import * as React from 'react';
-
-import { Card, CardContent, Grid, Typography } from '@mui/material';
+import { Card, CardContent, Grid, Modal, Typography } from '@mui/material';
 import { isRemoteAtom, needsJsonAtom } from '../../shared/atoms';
 
+import { Box } from '@mui/system';
+import Editor from '@monaco-editor/react';
 import { useRecoilValue } from 'recoil';
+import { useState } from 'react';
+import { useTheme } from '@emotion/react';
 
 export default function Cards() {
   const needsJson = useRecoilValue(needsJsonAtom);
@@ -22,18 +24,67 @@ export default function Cards() {
     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
       {result.map((need) => (
         <Grid item xs={3} sm={3} md={2} lg={1} xl={1} key={need.id}>
-          <Card>
-            <CardContent>
-              <Typography variant="body2" fontWeight={500}>
-                {need.id}
-              </Typography>
-              <Typography sx={{ mt: 1 }} color="text.secondary">
-                {need.title}
-              </Typography>
-            </CardContent>
-          </Card>
+          <SingleCard need={need} />
         </Grid>
       ))}
     </Grid>
+  );
+}
+
+function SingleCard(props) {
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const options = {
+    readOnly: true,
+    minimap: { enabled: false },
+    wordWrap: 'on'
+  };
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '800px',
+    height: '40vh',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    pt: 1
+  };
+  return (
+    <>
+      <Card onClick={handleOpen}>
+        <CardContent>
+          <Typography variant="body2" fontWeight={500}>
+            {props.need.id}
+          </Typography>
+          <Typography sx={{ mt: 1 }} color="text.secondary">
+            {props.need.title}
+          </Typography>
+        </CardContent>
+      </Card>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Editor
+            height="100%"
+            width="100%"
+            defaultLanguage="restructuredtext"
+            theme={theme.palette.mode === 'dark' ? 'vs-dark' : 'light'}
+            options={options}
+            defaultValue={`
+.. warn:: A warning to you!
+
+This is **bold** and *italic* text. This is **bold** and *italic* text.This is **bold** and *italic* text. This is **bold** and *italic* text.
+`}
+          />
+        </Box>
+      </Modal>
+    </>
   );
 }
