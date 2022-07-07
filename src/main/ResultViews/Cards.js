d3d5ff9492d3cb23158d58f25f7978110037296a
Marco Heinemann <marco.heinemann@useblocks.com>
2022-07-07 20:28:57 +0200

    Added Object.values to needs

--- src/main/ResultViews/Cards.js
+++ src/main/ResultViews/Cards.js
@@ -13,7 +13,7 @@ export default function Cards() {
   let result = [];
   if (needsJson && !isRemote) {
     var version = needsJson['current_version'];
-    result = needsJson['versions'][version]['needs'];
+    result = Object.values(needsJson['versions'][version]['needs']);
   }
 
   return (
